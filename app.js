ec2e5f02b17bea2f85541d8b179f6cc9dd8a5332
krosario26 <fencegames@gmail.com>
2014-10-25 23:10:49 -0400

    AlluringAd

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var routes = require('./routes');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/', routes.index);
+app.get('/about', routes.about);
+app.get('/services', routes.services);
+app.get('/portfolio', routes.portfolio);
+app.get('/contact', routes.contact);
+
+var server = app.listen(3000, function() {
+	console.log('Listening on port 3000');
+});
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+exports.index = function(req, res) {
+	res.render('index');
+}
+
+exports.about = function(req, res) {
+	res.render('about');
+}
+
+exports.services = function(req, res) {
+	res.render('services');
+}
+
+exports.portfolio = function(req, res) {
+	res.render('portfolio');
+}
+
+exports.contact = function(req, res) {
+	res.render('contact');
+}
