34dc1703aa05325ea0ad3b52828b46aaccdea447
krosario26 <fencegames@gmail.com>
2014-11-06 23:34:04 -0500

    And more..

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
-var favicon = require('serve-favicon');
 var routes = require('./routes');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-app.use(favicon(__dirname + '/public/images/logo.ico'));
 
 
 
