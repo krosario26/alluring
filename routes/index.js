b57e8280ace0fc9dfd8079efee3e85735911eb79
krosario26 <fencegames@gmail.com>
2014-11-18 16:45:53 -0500

    New Titles

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,31 @@
 exports.index = function(req, res) {
-	res.render('index');
+	res.render('index', {title: 'Home | Alluring Advertising'});
 }
 
 exports.about = function(req, res) {
-	res.render('about');
+	res.render('about', {title: 'About Us | Alluring Advertising'});
 }
 
 exports.services = function(req, res) {
-	res.render('services');
+	res.render('services', {title: 'Services | Alluring Advertising'});
 }
 
 exports.portfolio = function(req, res) {
-	res.render('portfolio');
-}
-
-exports.contact = function(req, res) {
-	res.render('contact');
+	res.render('portfolio', {title: 'Portfolio | Alluring Advertising'});
 }
 
 exports.webdesign = function(req, res) {
-	res.render('webdesign');
+	res.render('webdesign', {title: 'Portfolio | Alluring Advertising'});
 }
 
 exports.branding = function(req, res) {
-	res.render('branding');
+	res.render('branding', {title: 'Portfolio | Alluring Advertising'});
 }
 
 exports.print = function(req, res) {
-	res.render('print');
+	res.render('print', {title: 'Portfolio | Alluring Advertising'});
 }
 
 exports.clients = function(req, res) {
-	res.render('clients');
+	res.render('clients', {title: 'Portfolio | Alluring Advertising'});
 }
